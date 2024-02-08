dcc5aafcd08032609a4010f6141ff3faf8e443ba
João Manoel <joaomanoelmartinez8@gmail.com>
2024-02-08 10:50:11 -0300

    feat: Implementing Validator

---
+++ validator_raw.py
@@ -0,0 +1,32 @@
+from cerberus import Validator
+
+body = {
+    "data": {
+        "elemento1": 123.98,
+        "elemento2": "olaMundo",
+    }
+}
+
+# if "name" in body["data"]
+# if isinstance(body["data"]["name"], int)
+
+body_validator = Validator({
+    "data": {
+        "type": "dict",
+        "schema": {
+            "elemento1": { "type": "float", "required": True, "empty": False },
+            "elemento2": { "type": "string", "required": True, "empty": True },
+            "elemento3": { "type": "string", "required": False, "empty": False },
+        }
+    }
+})
+
+response = body_validator.validate(body)
+
+if response is False:
+    print(body_validator.errors)
+else:
+    print('Body OK')
+
+# print(repsonse)
+# print(body_validator.errors)
